64b64f1364e83dd91754b51509f166e63bba9c01
Luis Fernando Negron <lfernando_negron@hotmail.com>
2023-10-13 13:32:53 -0600

    Inyección de dependencias
    Se agregan implementaciones a las interfaces generadas

--- src/app/Data/Data.Module.ts
+++ src/app/Data/Data.Module.ts
@@ -5,6 +5,14 @@ import { ICiudadanoRepository } from "../Core/Interfaces/repositories/ICiudadano
 import { CiudadanoRepository } from "./Repository/Ciudadano.Repository";
 import { IDomicilioRepository } from "../Core/Interfaces/repositories/IDomicilio.Repository";
 import { DomicilioRepository } from "./Repository/Domicilio.Repository";
+import { IMenuRepository } from "../Core/Interfaces/repositories/Admin/IMenu.Repository";
+import { MenuRepository } from "./Repository/Admin/Menu.Repository";
+import { IProfileRepository } from "../Core/Interfaces/repositories/Admin/IProfile.Repository";
+import { ProfileRepository } from "./Repository/Admin/Profiles.Repository";
+import { IPersonRepository } from "../Core/Interfaces/repositories/Person/IPerson.Repsitory";
+import { PersonRepository } from "./Repository/Person/Person.Repository";
+import { IUserRepository } from "../Core/Interfaces/repositories/Person/IUser.Repository";
+import { UserRepository } from "./Repository/Person/User.Repository";
 
 @NgModule({
   imports: [
@@ -13,6 +21,10 @@ import { DomicilioRepository } from "./Repository/Domicilio.Repository";
   providers: [
     { provide: ICiudadanoRepository, useClass: CiudadanoRepository },
     { provide: IDomicilioRepository, useClass: DomicilioRepository },
+    { provide: IMenuRepository, useClass: MenuRepository },
+    { provide: IProfileRepository, useClass: ProfileRepository },
+    { provide: IPersonRepository, useClass: PersonRepository },
+    { provide: IUserRepository, useClass: UserRepository },
   ]
 })
 export class DataModule { }
--- src/app/Services/Services.Module.ts
+++ src/app/Services/Services.Module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { ICiudadanoService } from '../Core/Interfaces/services/ICiudadano.Service';
 import { CiudadanoService } from './Ciudadano.Service';
+import { IDomicilioService } from '../Core/Interfaces/services/IDomicilio.Service';
+import { DomicilioService } from './Domicilio.Service';
+import { IMenuService } from '../Core/Interfaces/services/Admin/IMenu.Service';
+import { MenuService } from './Admin/Menu.Service';
+import { IProfileService } from '../Core/Interfaces/services/Admin/IProfile.Service';
+import { ProfileService } from './Admin/Profile.Service';
+import { IPersonService } from '../Core/Interfaces/services/Person/IPerson.Service';
+import { PersonService } from './Person/Person.Service';
+import { IUserService } from '../Core/Interfaces/services/Person/IUser.Service';
+import { UserService } from './Person/User.Service';
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  exports: [],
   providers: [
     { provide: ICiudadanoService, useClass: CiudadanoService },
+    { provide: IDomicilioService, useClass: DomicilioService },
+    { provide: IMenuService, useClass: MenuService },
+    { provide: IProfileService, useClass: ProfileService },
+    { provide: IPersonService, useClass: PersonService },
+    { provide: IUserService, useClass: UserService },
   ]
 })
 export class ServicesModule { }
